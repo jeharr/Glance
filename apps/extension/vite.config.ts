5c175c4a3d6b5a1d716918d970c4b45045c37539
Jeffrey Harrison <jeffreyharrison@192.168.0.4>
2026-02-04 20:54:10 -0800

    successfully loaded extension into chrome browser

---
+++ apps/extension/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        content: resolve(__dirname, 'src/content.ts'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        format: 'iife',
+      },
+    },
+  },
+})
